36bf7cb0d2a183a532f0a039eee4a986b4cf99e4
suxiaoman <32409078+lyamango@users.noreply.github.com>
2018-03-07 22:44:26 +0800

    删除个人调试所用信息

--- app01/infrastructure/utilities/messages.py
+++ app01/infrastructure/utilities/messages.py
@@ -12,17 +12,17 @@ def email(email_list,content,subject="新浪微博用户注册"):
     # 三个参数：第一个为文本内容，第二个 plain 设置文本格式，第三个 utf-8 设置编码
     msg = MIMEText(content, 'plain', 'utf-8')
 
-    msg['From'] = formataddr(["新浪科技","minyite@163.com"])
+    msg['From'] = formataddr(["新浪科技","example@example.com"])
     msg['to'] = formataddr(["You",email_list])
     msg['Subject'] = subject
 
     try:
         print("正在尝试发送邮件")
         server = smtplib.SMTP()
-        server.connect("smtp.163.com", 25)
+        server.connect("example@example.com", 25)
 
-        server.login("minyite@163.com","su123456789") #两个参数，账号和密码
-        server.sendmail('minyite@163.com',email_list,msg.as_string())
+        server.login("example@example.com","yourpassword") #两个参数，账号和密码
+        server.sendmail('example@example.com',email_list,msg.as_string())
         print("邮件发送成功")
     except smtplib.SMTPException:
         print("Error：无法发送邮件")
